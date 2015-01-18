57b1d32e73252a47813f9025b712e9fb41d74298
Eli Van Zoeren <eli@elivz.com>
2015-01-17 22:19:25 -0800

    Document map tags

--- README.md
+++ README.md
@@ -36,6 +36,43 @@ Standard address format, with line-breaks at the end of each line.
     {{ entry.fieldName.country }} // Abbreviation, e.g. "US"
     {{ entry.fieldName.countryName }} // Full name, e.g. "United States"
 
+### Link to the address on an external mapping service
+
+    {{ entry.fieldName.mapUrl('google', { zoom: 5 }) }}
+
+The first parameter is the service to use, one of `google`, `yahoo`, `bing`, or `mapquest`. The second parameter is a hash of paramters to be passed through in the map URL. Please see the documentation for the particular mapping provider you are using for the available options.
+
+### Output the URL of a static map image from Google Maps
+
+    {% set mapParams = {
+        width: '400',
+        height: '200',
+        type: 'satellite'
+    } %}
+    {{ entry.fieldName.staticMapUrl(mapParams) }}
+
+The available parameters include:
+
+* `width` and `height` - The size in pixels of the image that is generated. (default: 400 x 200)
+* `scale` - Number of pixels returned. Set this to `2` for retina/hidpi support. (default: 1)
+* `zoom` - Zoom level of the map. (default: 14)
+* `format` - Specifies the image format to return. One of: png, png32, jpg, jpg-baseline, or gif. (default: png)
+* `type` - One of: roadmap, satellite, hybrid, or terrain. (default: roadmap)
+* `markerSize` - The relative size of the pushpin that marks the address location. One of: normal, mid, small, or tiny. (default: normal)
+* `markerColor` - The color of the pushpin. Either a named color (black, brown, green, purple, yellow, blue, gray, orange, red, or white) or a 6-digit hex-code, like "#ff0000". Three-digit color codes are not supported. (default: red)
+* `markerLabel` - Instead of the normal dot, you can specify a single letter or number to appear on the pushpin. (default: none)
+
+### Output an image tag containing the static Google Map
+
+    {% set mapParams = {
+        width: '400',
+        height: '200',
+        type: 'satellite'
+    } %}
+    {{ entry.fieldName.staticMap(mapParams) }}
+
+Accepts the same parameters as `staticMapUrl`.
+
 
 Installation
 ------------
--- vzaddress/models/VzAddress_AddressModel.php
+++ vzaddress/models/VzAddress_AddressModel.php
@@ -47,13 +47,12 @@ class VzAddress_AddressModel extends BaseModel
 
     public function plainHtml()
     {
-        return str_replace("\n", '<br>', $this->plainText());
+        $output = str_replace("\n", '<br>', $this->plainText());
+        return TemplateHelper::getRaw($output);
     }
 
-    public function mapUrl($params=array())
+    public function mapUrl($source='google', $params=array())
     {
-        $source = isset($params['source']) ? strtolower($params['source']) : 'google';
-        unset($params['source']);
         $params = count($params) ? '&' . http_build_query($params) : '';
 
         // Create the url-encoded address
@@ -76,6 +75,57 @@ class VzAddress_AddressModel extends BaseModel
         }
 
         return $output;
+    }
+
+    public function staticMapUrl($params=array())
+    {
+        $source = isset($params['source']) ? strtolower($params['source']) : 'google';
+        $width  = isset($params['width']) ? strtolower($params['width']) : '400';
+        $height = isset($params['height']) ? strtolower($params['height']) : '200';
+        $scale  = isset($params['scale']) ? strtolower($params['scale']) : '1';
+        $zoom   = isset($params['zoom']) ? strtolower($params['zoom']) : '14';
+        $format = isset($params['format']) ? strtolower($params['format']) : 'png';
+        $type   = isset($params['type']) ? strtolower($params['type']) : 'roadmap';
+        $size   = isset($params['markerSize']) ? strtolower($params['markerSize']) : FALSE;
+        $label  = isset($params['markerLabel']) ? strtoupper($params['markerLabel']) : FALSE;
+        $color  = isset($params['markerColor']) ? strtolower($params['markerColor']) : FALSE;
+
+        // Normalize the color parameter
+        $color = str_replace('#', '0x', $color);
+
+        // Create the url-encoded address
+        $address = urlencode(implode(', ', $this->toArray()));
+
+        $output = isset($params['secure']) && $params['secure'] == 'yes' ? 'https' : 'http';
+        $marker = '';
+        switch ($source)
+        {
+            case 'yahoo':
+                // TODO
+            case 'bing':
+                // TODO
+            case 'mapquest':
+                // TODO
+            case 'google': default:
+                $marker .= $size ? 'size:'.$size.'|' : '';
+                $marker .= $color ? 'color:'.$color.'|' : '';
+                $marker .= $label ? 'label:'.$label.'|' : '';
+                $output .= "://maps.googleapis.com/maps/api/staticmap?zoom={$zoom}&size={$width}x{$height}&scale={$scale}&format={$format}&maptype={$type}&markers={$marker}{$address}&sensor=false";
+                break;
+        }
+
+        return $output;
+    }
+
+    public function staticMap($params=array())
+    {
+        $width  = isset($params['width']) ? strtolower($params['width']) : '400';
+        $height = isset($params['height']) ? strtolower($params['height']) : '200';
+        $map_url = $this->staticMapUrl($params);
+        $address = htmlspecialchars($this->inline());
+
+        $output = '<img src="'.$map_url.'" alt="'.$address.'" width="'.$width.'" height="'.$height.'">';
+        return TemplateHelper::getRaw($output);
     }
 
     /**
