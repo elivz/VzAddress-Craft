4a90f9574fdd573737390feb15969a48ce90b987
Josh Crawford <joshua@sgroup.com.au>
2015-11-27 01:29:59 +1100

    Add Feed Me importing hooks for Address fieldtype, and include multiple options html
    Signed-off-by: Josh Crawford <joshua@sgroup.com.au>

--- vzaddress/VzAddressPlugin.php
+++ vzaddress/VzAddressPlugin.php
@@ -25,4 +25,38 @@ class VzAddressPlugin extends BasePlugin
     {
         return 'http://elivz.com';
     }
+
+    public function registerFeedMeMappingOptions()
+    {
+        return array(
+            'VzAddress' => 'vzaddress/_plugins/feedMeOptions',
+        );
+    }
+
+    public function prepForFeedMeFieldType($field, &$data, $handle)
+    {
+        // Ensure it's a VzAddress field
+        if ($field->type == 'VzAddress') {
+
+            // Initialize content array
+            $content = array();
+
+            // Separate field handle & subfield handle
+            if (preg_match('/^(.*)\[(.*)]$/', $handle, $matches)) {
+                $fieldHandle    = $matches[1];
+                $subFieldHandle = $matches[2];
+
+                // Ensure address array exists
+                if (!array_key_exists($fieldHandle, $content)) {
+                    $content[$fieldHandle] = array();
+                }
+
+                // Set value to subfield of correct address array
+                $content[$fieldHandle][$subFieldHandle] = $data[$fieldHandle];
+            }
+
+            // Modify data
+            $data = $content;
+        }
+    }
 }
