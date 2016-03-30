b056d3470660dd5f9a5513a41ac66edf9958284e
Eli Van Zoeren <eli@elivz.com>
2016-03-29 23:17:02 -0700

    Bump version number

--- vzaddress/VzAddressPlugin.php
+++ vzaddress/VzAddressPlugin.php
@@ -11,7 +11,7 @@ class VzAddressPlugin extends BasePlugin
     }
 
     public function getVersion() {
-        return '1.0.2';
+        return '1.0.3';
     }
 
     public function getDeveloper() {
