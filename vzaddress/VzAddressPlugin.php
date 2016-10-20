19ffb3f2f9ed7f52727eecd18a0dd565471b2d72
Tim Kelty <tim@fusionary.com>
2016-10-20 12:24:28 -0400

    Add "Import" Plugin compatibility https://github.com/boboldehampsink/import

--- vzaddress/VzAddressPlugin.php
+++ vzaddress/VzAddressPlugin.php
@@ -33,4 +33,36 @@ class VzAddressPlugin extends BasePlugin
     public function getReleaseFeedUrl() {
         return 'https://raw.githubusercontent.com/elivz/VzAddress-Craft/master/changelog.json';
     }
+
+    public function registerImportOptionPaths()
+    {
+        return array(
+            'VzAddress' => 'vzaddress/integrations/import/options',
+        );
+    }
+
+    public function modifyImportRow($element, $map, $data)
+    {
+        $rowData = array_combine($map, $data);
+        $content = [];
+
+        foreach ($rowData as $key => $value) {
+            if (preg_match('/^(.*)\[(.*)]$/', $key, $matches)) {
+                $handle   = $matches[1];
+                $subHandle = $matches[2];
+                $field = craft()->fields->getFieldByHandle($handle);
+
+                if ($field->fieldType->classHandle === 'VzAddress') {
+                    if (!array_key_exists($handle, $content)) {
+                        $content[$handle] = array();
+                    }
+
+                    $content[$handle][$subHandle] = $value;
+                }
+            }
+        }
+
+        // Set new content
+        $element->setContentFromPost($content);
+    }
 }
