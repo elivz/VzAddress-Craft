33ef623f169683fabfebd64dc6edd021d0d3cf36
Eli Van Zoeren <eli@elivz.com>
2016-10-23 21:10:30 -0400

    Merge support for Import plugin

--- vzaddress/VzAddressPlugin.php
+++ vzaddress/VzAddressPlugin.php
@@ -11,7 +11,7 @@ class VzAddressPlugin extends BasePlugin
     }
 
     public function getVersion() {
-        return '1.0.6';
+        return '1.1.0';
     }
 
     public function getSchemaVersion() {
